96d835f0bbd0a22dcfefaa5261d5357206db3ffe
Obr00007576 <51303014+Obr00007576@users.noreply.github.com>
2023-05-03 20:07:19 +0800

    0.0.2

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ env = gym.make("LunarLander-v2", render_mode='human')
 observation, info = env.reset(seed=42) 
 n = 0
 reward_sum = 0
-while n < 1:
+while n < 5:
     action = model.get_action(observation)
     new_observation, reward, terminated, truncated, info = env.step(action)
     reward_sum += reward
@@ -21,5 +21,5 @@ while n < 1:
         n+=1
         print(f'epoch {n}: {reward_sum}')
         reward_sum=0
-        observation, info = env.reset()
+        observation, info = env.reset(seed=42)
 env.close()
